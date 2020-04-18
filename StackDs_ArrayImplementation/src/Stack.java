b636c8014af7e8e05cf1521590184e1adbe56dd1
Prosper Ekwerike <43956851+Pekwerike@users.noreply.github.com>
2020-04-18 01:51:04 +0100

    Update Stack.java

--- StackDs_ArrayImplementation/src/Stack.java
+++ StackDs_ArrayImplementation/src/Stack.java
@@ -1,18 +1,34 @@
 public class Stack {
     /**Declaring the array for the stack**/
     String[] stackArray;
+    /**Array to hold values during the resizing process**/
+    String [] tempCopyArray;
     int N = 0;
-    /**Constructor for the client to initialize the size of the array**/
-    Stack(int stackSize){
-        stackArray = new String[stackSize];
+
+    /**Function that resizes the stack array based on the new capacity**/
+    public void resizingTheArray(int capacity){
+       tempCopyArray = new String[capacity];
+        for(int i = 0; i < stackArray.length; i++){
+            tempCopyArray[i] = stackArray[i];
+        }
+        stackArray = tempCopyArray;
+    }
+    /**Constructor to initializeof the array**/
+    Stack(){
+        stackArray = new String[1];
     }
 
+    /**function to check if the array is empty**/
     public Boolean isEmpty(){
         return stackArray[0] == null;
     }
 
     /**Pushing elements into the stack**/
     public void push(String inputForStack){
+        /**If the array is full, double the size**/
+        if (N == stackArray.length){
+            resizingTheArray(2 * stackArray.length);
+        }
         /**Use the value N to index into the array, then increement N value by 1**/
         stackArray[N++] = inputForStack;
     }
@@ -23,13 +39,18 @@ public class Stack {
         if (isEmpty()) {
             return "The stack is empty";
         } else {
-            /**Return the top most element in the array, by decreementing the value of N by 1, and then using it**/
+            /**If the array is 1/4 full, reduce the size to half the original size**/
+            if(N == stackArray.length/4){
+                resizingTheArray(stackArray.length/2);
+            }
+             /**Return the top most element in the array, by decreementing the value of N by 1, and then using it**/
             String dataPopped = stackArray[--N];
             /**Set the value in the index of the pop item to null
              * this is to avoid loitering i.e holding reference to a value that is no longer in us.
              * Now the garbage collector can proceed to reclaim the memory**/
             stackArray[N] = null;
             return dataPopped;
-        }
+
     }
+}
 }
